78a05692da3285bc7b4646ab2288b59188919d42
jun.chu <1571970439@qq.com>
2017-06-20 23:06:38 +0800

    finished

--- src/main/java/com/chujun/spring/boot/controller/SampleController.java
+++ src/main/java/com/chujun/spring/boot/controller/SampleController.java
@@ -1,24 +1,15 @@
 package com.chujun.spring.boot.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by chujun on 2017/6/17.
  */
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class SampleController {
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args){
-        SpringApplication.run(SampleController.class,args);
+        return "Greetings from Spring Boot!";
     }
 }
